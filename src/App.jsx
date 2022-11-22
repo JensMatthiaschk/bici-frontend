f9eeaceecf93d38653994693eb098fd212f9a815
JensMatthiaschk <jens.matthiaschk@gmail.com>
2022-11-22 14:22:04 +0100

    add navbar sample

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
+import Navbar from './components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
+      <Navbar />
       <h1>Hello World!</h1>
     </div>
   )
