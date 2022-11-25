9d616609e0659d9fad3a17383536b0ccd976f4ca
JensMatthiaschk <jens.matthiaschk@gmail.com>
2022-11-25 18:17:02 +0100

    add userprofile input and and backend connection

--- src/routes/Errorpage.jsx
+++ src/routes/Errorpage.jsx
@@ -4,7 +4,7 @@ import { useRouteError } from "react-router-dom";
 export default function ErrorPage() {
 
     const error = useRouteError();
-    console.log(error)
+
     return (
 
         <div>
