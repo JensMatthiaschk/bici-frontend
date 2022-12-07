4c68d8660ceca020eb9c892e36a0e6e72c1089f2
fabian-elmar <fabsiban09@gmail.com>
2022-12-07 15:49:34 +0100

    image on pins

--- src/components/map/Drawer.jsx
+++ src/components/map/Drawer.jsx
@@ -1,135 +1,161 @@
-
-import { useContext, useState } from "react"
-import { MapContext } from "../mapContext.jsx"
-import PinComments from "./PinComments.jsx"
-import { PinRating } from "./PinRating.jsx"
-import { FaStar } from "react-icons/fa"
-import { useEffect } from "react"
-
-
+import { useContext, useState } from "react";
+import { MapContext } from "../mapContext.jsx";
+import PinComments from "./PinComments.jsx";
+import { PinRating } from "./PinRating.jsx";
+import { FaStar } from "react-icons/fa";
+import { useEffect } from "react";
 
 export const Drawer = () => {
-    const { pinId, setPinId, pinRatings } = useContext(MapContext)
-    const [ratingInput, setRatingInput] = useState([])
-    const [averageRating, setAverageRating] = useState(4)
-    const [totalRating, setTotalratings] = useState()
+  const { pinId, setPinId, pinRatings, mapPins, drawerPin } =
+    useContext(MapContext);
+  const [ratingInput, setRatingInput] = useState([]);
+  const [averageRating, setAverageRating] = useState(4);
+  const [totalRating, setTotalratings] = useState();
+  const [pin, setPin] = useState([]);
 
+  const filterColor = {
+    camping: "text-white bg-red-800",
+    events: " bg-yellow-400",
+    host: "text-white bg-fuchsia-800",
+    repair: " bg-amber-500",
+    shower: "text-white bg-emerald-600",
+    swim: "text-white bg-blue-700",
+  };
 
-    const Images = [
-        {
-            avatar_img: "https://biciappimages.s3.eu-central-1.amazonaws.com/pin_images/Placeholder_view_vector.svg.png"
-        },
-        {
-            avatar_img: "https://biciappimages.s3.eu-central-1.amazonaws.com/pin_images/Placeholder_view_vector.svg.png"
-        },
-        {
-            avatar_img: "https://biciappimages.s3.eu-central-1.amazonaws.com/pin_images/Placeholder_view_vector.svg.png"
-        },
-        {
-            avatar_img: "https://biciappimages.s3.eu-central-1.amazonaws.com/pin_images/Placeholder_view_vector.svg.png"
-        }
-    ]
+  useEffect(() => {
+    if (pinRatings) {
+      pinRatings.map((e) => setRatingInput([...pinRatings, e.ratingValue]));
+    }
+    console.log("pinRatings", pinRatings);
+  }, [pinId]);
 
-    useEffect(() => {
-        if (pinRatings) {
-            pinRatings.map((e) =>
-                setRatingInput([...pinRatings, e.ratingValue])
-            )
-        }
-        console.log("pinRatings", pinRatings)
-    }, [pinId])
+  useEffect(() => {
+    if (pinRatings?.length) {
+      let tot = 0;
+      pinRatings.forEach((e) => (tot += e.ratingValue));
+      setAverageRating(tot / pinRatings.length);
+    }
+  }, [pinRatings]);
 
-    useEffect(() => {
-        if (pinRatings?.length) {
-            let tot = 0;
-            pinRatings.forEach((e) => tot += e.ratingValue)
-            setAverageRating(tot / pinRatings.length)
-        }
-    }, [pinRatings])
+  useEffect(() => {
+    console.log("OOOOOOOO", averageRating);
+  }, [averageRating]);
 
-    useEffect(() => {
-        console.log("OOOOOOOO", averageRating)
-    }, [averageRating])
-
-
-    return (
-
-        <div className="drawer-side ">
-            <label htmlFor="my-drawer" className="drawer-overlay"></label>
-            <ul className="menu w-80 bg-base-100 text-base-content">
-                {/* Carousel */}
-                <div className="carousel w-full h-60 mb-10">
-                    {Images.map((img, i) => {
-                        const count = i + 1
-                        return (
-                            <div id={"drawerSlide" + `${count}`}
-                                style={{
-                                    "background-image": `url("https://biciappimages.s3.eu-central-1.amazonaws.com/pin_images/Placeholder_view_vector.svg.png")`,
-                                    "background-repeat": "no-repeat",
-                                    "background-size": "cover",
-                                }}
-                                className="carousel-item relative w-full">
-                                {/* <img src="https://placeimg.com/800/200/arch" className="w-full" /> */}
-                                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                                    <a href={"#drawerSlide" + `${count - 1}`} className="btn btn-circle w-8 h-4 p-0">❮</a>
-                                    <a href={"#drawerSlide" + `${count + 1}`} className="btn btn-circle w-8 h-4 p-0">❯</a>
-                                </div>
-                            </div>
-                        )
-                    })}
+  return (
+    <div className="drawer-side ">
+      <label htmlFor="my-drawer" className="drawer-overlay"></label>
+      <ul className="menu w-80 bg-base-100 text-base-content">
+        {/* Carousel */}
+        <div className="carousel w-full h-60 mb-10">
+          {drawerPin.pin_imgs.map((img, i) => {
+            const count = i + 1;
+            return (
+              <div
+                id={"drawerSlide" + `${count}`}
+                style={{
+                  "background-image": `url(${
+                    img.aws_url !== null
+                      ? img.aws_url
+                      : "https://biciappimages.s3.eu-central-1.amazonaws.com/pin_images/Placeholder_view_vector.svg.png"
+                  })`,
+                  "background-repeat": "no-repeat",
+                  "background-size": "cover",
+                }}
+                className="carousel-item relative w-full"
+              >
+                {/* <img src="https://placeimg.com/800/200/arch" className="w-full" /> */}
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                  <a
+                    href={"#drawerSlide" + `${count - 1}`}
+                    className="btn btn-circle w-8 h-4 p-0"
+                  >
+                    ❮
+                  </a>
+                  <a
+                    href={"#drawerSlide" + `${count + 1}`}
+                    className="btn btn-circle w-8 h-4 p-0"
+                  >
+                    ❯
+                  </a>
                 </div>
-
-
-                {/* Rating Average */}
-                <>
-                    <div className="flex relative bottom-4 items-center p-4">
-                        <br />
-                        {averageRating ? <p className="pr-2">{averageRating}</p> : <p className="pr-2">0.0</p>}
-                        {[...Array(5)].map((star, i) => {
-                            const ratingValueCurrent = i + 1
-                            if (pinRatings) {
-                                return (
-                                    <label>
-                                        <input
-                                            name="rating"
-                                            type="radio"
-                                            value={ratingValueCurrent}
-                                        />
-                                        <FaStar
-                                            className="star"
-                                            color={ratingValueCurrent <= (averageRating) ? "rgb(251, 146, 60)" : "grey"}
-                                            size={30}
-                                        />
-                                    </label>
-                                )
-                            } else {
-                                return (
-                                    <label>
-                                        <input
-                                            name="rating"
-                                            type="radio"
-                                            value={"0"}
-                                        />
-                                        <FaStar
-                                            className="star"
-                                            color={"grey"}
-                                            size={30}
-                                        />
-                                    </label>
-                                )
-                            }
-                        })
-                        }
-                        {pinRatings ? <p className="pr-2">({pinRatings.length})</p> : <p className="pr-2">(0)</p>}
-                    </div >
-                </>
-
-                {/* User Input */}
-                <PinComments pin={pinId} />
-                <PinRating />
-                {/* <>{pinId}</> */}
-
-            </ul>
+              </div>
+            );
+          })}
         </div>
-    )
-}
+
+        {/* Rating Average */}
+        <>
+          <div className="flex relative bottom-4 items-center p-2">
+            <br />
+            {averageRating ? (
+              <p className="pr-2">{averageRating}</p>
+            ) : (
+              <p className="pr-2">0.0</p>
+            )}
+            {[...Array(5)].map((star, i) => {
+              const ratingValueCurrent = i + 1;
+              if (pinRatings) {
+                return (
+                  <label>
+                    <input
+                      name="rating"
+                      type="radio"
+                      value={ratingValueCurrent}
+                    />
+                    <FaStar
+                      className="star"
+                      color={
+                        ratingValueCurrent <= averageRating
+                          ? "rgb(251, 146, 60)"
+                          : "grey"
+                      }
+                      size={30}
+                    />
+                  </label>
+                );
+              } else {
+                return (
+                  <label>
+                    <input name="rating" type="radio" value={"0"} />
+                    <FaStar className="star" color={"grey"} size={30} />
+                  </label>
+                );
+              }
+            })}
+            {pinRatings ? (
+              <p className="pr-2">({pinRatings.length})</p>
+            ) : (
+              <p className="pr-2">(0)</p>
+            )}
+          </div>
+        </>
+
+        {/* User Input */}
+        <div>
+          {" "}
+          <p className="text-lg pl-2">{drawerPin.title}</p>
+          <div className="pl-4">
+            {
+              //filter for pin where value is true and return the key of the object
+              Object.keys(drawerPin)
+                .filter((key) => drawerPin[key] === true)
+                .map((key) => {
+                  return (
+                    <span
+                      className={`badge badge-ghost badge-sm flex-col ${filterColor[key]}`}
+                    >
+                      {key}
+                    </span>
+                  );
+                })
+            }
+          </div>
+        </div>
+        <p className="text-sm pl-2 pt-0">{drawerPin.description}</p>
+        <PinComments pin={pinId} />
+        <PinRating />
+        {/* <>{pinId}</> */}
+      </ul>
+    </div>
+  );
+};
--- src/routes/Profile.jsx
+++ src/routes/Profile.jsx
@@ -1,105 +1,132 @@
-import React, { useEffect } from 'react'
-import { useLoaderData, useNavigate } from 'react-router-dom'
-import { getProfileData, verifier } from '../authservice'
-import Navbar from '../components/Navbar'
-import ProfileDashboard from '../components/ProfileDashboard'
-import ProfileForm from './ProfileForm'
+import React, { useEffect } from "react";
+import { useLoaderData, useNavigate } from "react-router-dom";
+import { getProfileData, verifier } from "../authservice";
+import Navbar from "../components/Navbar";
+import ProfileDashboard from "../components/ProfileDashboard";
+import ProfileForm from "./ProfileForm";
 
 export async function loader() {
-    return getProfileData();
-    // const data = await getProfileData();
-    // console.log("loader", data);
-    // return data;
+  return getProfileData();
+  // const data = await getProfileData();
+  // console.log("loader", data);
+  // return data;
 }
 
 export const Profile = () => {
-    const navigate = useNavigate();
-    const profileData = useLoaderData();
-    console.log('ProfileData', profileData);
-    //const birthday = new Date(profileData.data.birthday).toLocaleTimeString('en-IN')
+  const navigate = useNavigate();
+  const profileData = useLoaderData();
+  console.log("ProfileData", profileData);
+  //const birthday = new Date(profileData.data.birthday).toLocaleTimeString('en-IN')
 
+  (async () => {
+    const token = await verifier();
+    // console.log('tok1', token)
+    if (!token.success) return navigate("/login");
+  })();
 
-    (async () => {
-        const token = await verifier();
-        // console.log('tok1', token)
-        if (!token.success) return navigate("/login");
-    })();
+  return (
+    <>
+      <Navbar />
 
-    return (<>
-        <Navbar />
-
-        <div className=' bg-gradient-to-b from-accent to-white'>
-            {/* <div className="chat chat-start">
+      <div className=" bg-gradient-to-b from-accent to-white">
+        {/* <div className="chat chat-start">
                 <div className="chat-bubble">It's over Anakin, <br />I have the high ground.</div>
             </div>
             <div className="chat chat-end">
                 <div className="chat-bubble chat-bubble-black">You underestimate my power!</div>
             </div> */}
-            {profileData.data ?
-                <div className=" text-center  mb-2">
-                    <div className="flex justify-center mb-5 pt-8 ">
-                        <img className=" w-24 h-3/5 rounded-full border-solid border-2 border-grey " src={`${profileData.data.avatar_img.aws_url}`} />
-                    </div>
-                    <div className=" px- text-black">
-                        {/* <h3 className='font-bold pb-4 text-base'>Account Details</h3> */}
+        {profileData.data ? (
+          <div className=" text-center  mb-2">
+            <div className="flex justify-center mb-5 pt-8 ">
+              <img
+                className=" w-24 h-3/5 rounded-full border-solid border-2 border-grey "
+                src={`${profileData.data.avatar_img.aws_url}`}
+              />
+            </div>
+            <div className=" px- text-black">
+              {/* <h3 className='font-bold pb-4 text-base'>Account Details</h3> */}
 
-                        {profileData.data.nickname !== "" ?
-                            <>
-                                <h6 className='font-bold pb-2 text-lg'>Welcome Back</h6>
-                                <p className='text-lg pb-4'>{profileData.data.nickname}</p>
-                            </> : null}
-
-                        {profileData.data.bikeType !== "" ?
-                            <>
-                                <h6 className='font-bold  pb-2 text-lg'>My bike style</h6>
-                                <p className='text-lg pb-4'>{profileData.data.bikeType}</p>
-                            </> : null}
-                        {profileData.data.description ?
-                            <>
-                                <h6 className='font-bold  pb-2 text-lg'>About me</h6>
-                                <p className='text-lg pb-4'> {profileData.data.description}</p>
-                            </>
-                            : null}
-                        {profileData.data.address ?
-                            <>
-                                <h6 className='font-bold pb-2 text-lg'>address</h6>
-                                <p className='text-lg pb-4'>{profileData.data.address}</p>
-                            </>
-                            : null}
-                        {profileData.data.cell ?
-                            <>
-                                <h6 className='font-bold  pb-2 text-lg'>cellphone number</h6>
-                                <p className='text-lg pb-4'>{profileData.data.cell}</p>
-                            </>
-                            : null}
-                        {profileData.data.birthday ?
-                            <>
-                                <h6 className='font-bold  pb-2 text-lg'>birthday</h6>
-                                <p className='text-lg pb-4'>{new Date(profileData.data.birthday).toLocaleDateString('en-IN')}</p>
-                            </>
-                            : null}
-                        {profileData.data.description === "" || profileData.data.address === "" || profileData.data.cell === "" || profileData.data.birthday === "" ?
-                            <>
-                                <p className='text-lg mt-4 mb-2'>Please complete filling out your profile details</p>
-                                <label htmlFor='ProfileForm' className="btn btn-lg">Edit Profile</label>
-                            </>
-                            : ""}
-                        <ProfileForm />
-                    </div>
-                </div>
-                :
+              {profileData.data.nickname !== "" ? (
                 <>
-                    <p className='text-sm mt-4 mb-2'>Please complete filling out your profile details</p>
-                    <label htmlFor='ProfileForm' className="btn btn-sm">Edit Profile</label>
-                    <ProfileForm />
+                  <h6 className="font-bold pb-2 text-lg">Welcome Back</h6>
+                  <p className="text-lg pb-4">{profileData.data.nickname}</p>
                 </>
-            }
-        </div>
-        <div className='flex justify-center place-content-around bg-white '>
-            <ProfileDashboard />
-        </div>
-    </>
-    )
+              ) : null}
 
-}
-export default Profile
+              {profileData.data.bikeType !== "" ? (
+                <>
+                  <h6 className="font-bold  pb-2 text-lg">My bike style</h6>
+                  <p className="text-lg pb-4">{profileData.data.bikeType}</p>
+                </>
+              ) : null}
+              {profileData.data.description ? (
+                <>
+                  <h6 className="font-bold  pb-2 text-lg">About me</h6>
+                  <p className="text-lg pb-4">
+                    {" "}
+                    {profileData.data.description}
+                  </p>
+                </>
+              ) : null}
+              {profileData.data.address ? (
+                <>
+                  <h6 className="font-bold pb-2 text-lg">address</h6>
+                  <p className="text-lg pb-4">{profileData.data.address}</p>
+                </>
+              ) : null}
+              {profileData.data.cell ? (
+                <>
+                  <h6 className="font-bold  pb-2 text-lg">cellphone number</h6>
+                  <p className="text-lg pb-4">{profileData.data.cell}</p>
+                </>
+              ) : null}
+              {profileData.data.birthday ? (
+                <>
+                  <h6 className="font-bold  pb-2 text-lg">birthday</h6>
+                  <p className="text-lg pb-4">
+                    {new Date(profileData.data.birthday).toLocaleDateString(
+                      "en-IN"
+                    )}
+                  </p>
+                </>
+              ) : null}
+              {profileData.data.description === "" ||
+              profileData.data.address === "" ||
+              profileData.data.cell === "" ||
+              profileData.data.birthday === "" ? (
+                <>
+                  <p className="text-lg mt-4 mb-2">
+                    Please complete filling out your profile details
+                  </p>
+                  <label htmlFor="ProfileForm" className="btn btn-lg">
+                    Edit Profile
+                  </label>
+                </>
+              ) : (
+                ""
+              )}
+              <ProfileForm />
+            </div>
+          </div>
+        ) : (
+          <>
+            <p className="text-sm mt-4 mb-2">
+              Please complete filling out your profile details
+            </p>
+            <label htmlFor="ProfileForm" className="btn btn-sm">
+              Edit Profile
+            </label>
+            <ProfileForm />
+          </>
+        )}
+      </div>
+      <p className="flex justify-center text-base">
+        Find your Pins and Comments down here:
+      </p>
+      <div className="flex justify-center place-content-around bg-white ">
+        <ProfileDashboard />
+      </div>
+    </>
+  );
+};
+export default Profile;
