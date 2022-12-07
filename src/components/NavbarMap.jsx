c5f0c1691c7d43946a9d9aa0172e594a14923cd8
fabian-elmar <fabsiban09@gmail.com>
2022-12-07 12:19:06 +0100

    filter on map

--- src/components/NavbarMap.jsx
+++ src/components/NavbarMap.jsx
@@ -1,44 +1,56 @@
-import React, { useContext } from 'react'
-import { MapContext } from './mapContext'
+import React, { useContext } from "react";
+import { useEffect } from "react";
+import { MapContext } from "./mapContext";
 
 const NavbarMap = () => {
-    const { filter, setFilter } = useContext(MapContext)
-    console.log('filter', filter)
-    //add array to filter   
+  const { filter, setFilter } = useContext(MapContext);
 
+  useEffect(() => console.log("filter", filter), [filter]);
 
-    const filterColor = {
-        camping: 'btn w-14 normal-case text-xl bg-red-800 border-0 ',
-        events: 'btn w-14 normal-case text-xl bg-yellow-400 border-0',
-        host: 'btn w-14 normal-case text-xl bg-fuchsia-800 border-0',
-        repair: 'btn w-14 normal-case text-xl bg-amber-500 border-0',
-        shower: 'btn w-14 normal-case text-xl bg-emerald-600 border-0',
-        swim: 'btn w-14 normal-case text-xl bg-blue-700 border-0',
-    }
-    const filterCK = Object.values(filterColor)
+  const filterColor = {
+    camping: `bg-red-800 rounded-l-lg`,
+    events: "bg-yellow-400 ",
+    host: "bg-fuchsia-800 border-l-2  border-black",
+    repair: "bg-amber-500 border-l-2  border-black",
+    shower: "bg-emerald-600 border-l-2  border-black",
+    swim: "bg-blue-700 rounded-r-lg border-l-2  border-black ",
+  };
+  const filterCK = Object.values(filterColor);
 
+  return (
+    <div className="border-2 border-black rounded-xl text-center">
+      {Object.keys(filter).map((key, fc) => {
+        console.log(key, filter[key], filterCK[fc]);
+        return (
+          <label
+            key={key}
+            className={`w-14 border-l-2  border-black normal-case text-xl 
+            ${key === "camping" && !filter[key] ? "rounded-l-lg" : ""}
 
-    return (
-        <div>
-            {Object.keys(filter).map((key, fc) => {
-                return (
-                    <label key={key} className={filterCK[fc]} >
+                
+                ${filter[key] ? filterCK[fc] : ""}`}
+          >
+            <span className="label-text text-xs text-center font-bold">
+              {key}
+            </span>
+            <input
+              type="checkbox"
+              className="checkbox mx-2 hidden "
+              id={key}
+              name={key}
+              checked={filter[key]}
+              onChange={(e) => {
+                setFilter({
+                  ...filter,
+                  [e.target.name]: !filter[e.target.name],
+                });
+              }}
+            />
+          </label>
+        );
+      })}
+    </div>
+  );
+};
 
-                        <span className="label-text">{key}</span>
-                        <input type="checkbox" className="checkbox checkbox-accent mx-2" id={key} name={key} value={key}
-                            checked={filter[key]}
-
-                            onChange={(e) => {
-                                setFilter({ ...filter, [e.target.name]: !filter[e.target.name] })
-                            }} />
-
-                    </label>)
-            })}
-
-
-
-        </div>
-    )
-}
-
-export default NavbarMap
+export default NavbarMap;
